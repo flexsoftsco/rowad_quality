837ab85fb4c1017387c984381850d72115574e2c
ashish-greycube <mr.ashish.shah@gmail.com>
2022-02-23 16:55:56 +0530

    feat: module landing page

---
+++ rowad_quality/config/rowad_quality.py
@@ -0,0 +1,51 @@
+from __future__ import unicode_literals
+from frappe import _
+
+
+def get_data():
+    config = [
+        {'label': _('Rowad Quality'), 
+        'items': [
+                {
+                    "type": "doctype",
+                    "name": "SOP",
+                    "label": _("SOP")
+                },
+                {
+                    "type": "doctype",
+                    "name": "Change Control Request",
+                    "label": _("Change Control Request")
+                },
+                {
+                    "type": "doctype",
+                    "name": "Quality Issues Request",
+                    "label": _("Quality Issues Request")
+                },
+                {
+                    "type": "doctype",
+                    "name": "Amount Request",
+                    "label": _("Amount Request")
+                }                                                
+            ]
+        },
+        {'label': _('Transaction'), 
+        'items': [
+                {
+                    "type": "doctype",
+                    "name": "Material Request",
+                    "label": _("Material Request")
+                },
+                {
+                    "type": "doctype",
+                    "name": "Payment Request",
+                    "label": _("Payment Request")
+                } ,
+                {
+                    "type": "doctype",
+                    "name": "Leave Application",
+                    "label": _("Leave Application")
+                }                                
+            ]
+        }        
+    ]
+    return config    
