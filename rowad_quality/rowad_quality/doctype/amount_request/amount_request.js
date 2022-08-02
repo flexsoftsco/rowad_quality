75e1fb4e650eb6fc768bd9cf8a9a50aa8c661374
vr-greycube <vijay@greycube.in>
2022-08-02 18:53:22 +0530

    feat: set party balance and total in amount request

--- rowad_quality/rowad_quality/doctype/amount_request/amount_request.js
+++ rowad_quality/rowad_quality/doctype/amount_request/amount_request.js
@@ -2,7 +2,23 @@
 // For license information, please see license.txt
 
 frappe.ui.form.on('Amount Request', {
-	// refresh: function(frm) {
+	employee: function (frm) {
+		frappe.call({
+			method: 'erpnext.accounts.utils.get_balance_on',
+			args: {
+				party_type: "Employee",
+				party: frm.doc.employee
+			}, callback: function (r) {
+				frm.set_value("party_balance", r.message)
+			}
+		})
+	},
 
-	// }
+	validate: function (frm) {
+		let total_amount = frm.doc.amount_request_detail.reduce(function (acc, item) { return acc + item.amount; }, 0);
+		frm.set_value("total_amount", total_amount)
+		frm.set_value("balance_after_request", total_amount - (frm.doc.party_balance || 0))
+	},
+
+
 });
