c60f2d19dbda6538874e10b759d8feea591b7171
vr-greycube <vijay@greycube.in>
2022-08-04 13:20:26 +0530

    minor: get party balance based on commpy settings employee petty cash account

--- rowad_quality/rowad_quality/doctype/amount_request/amount_request.js
+++ rowad_quality/rowad_quality/doctype/amount_request/amount_request.js
@@ -3,22 +3,28 @@
 
 frappe.ui.form.on('Amount Request', {
 	employee: function (frm) {
-		frappe.call({
-			method: 'erpnext.accounts.utils.get_balance_on',
-			args: {
-				party_type: "Employee",
-				party: frm.doc.employee
-			}, callback: function (r) {
-				frm.set_value("party_balance", r.message)
-			}
+
+		frappe.db.get_value('Company',
+			frappe.defaults.get_default('company'), 'default_employee_petty_cash_payable_account_cf'
+		).then((result) => {
+			frappe.call({
+				method: 'erpnext.accounts.utils.get_balance_on',
+				args: {
+					account: result.message.default_employee_petty_cash_payable_account_cf,
+					date: frappe.datetime.get_today(),
+					party_type: "Employee",
+					party: frm.doc.employee
+				}, callback: function (r) {
+					frm.set_value("party_balance", r.message)
+				}
+			})
 		})
+
 	},
 
 	validate: function (frm) {
 		let total_amount = frm.doc.amount_request_detail.reduce(function (acc, item) { return acc + item.amount; }, 0);
 		frm.set_value("total_amount", total_amount)
-		frm.set_value("balance_after_request", total_amount - (frm.doc.party_balance || 0))
+		frm.set_value("balance_after_request", total_amount + (frm.doc.party_balance || 0))
 	},
-
-
 });
